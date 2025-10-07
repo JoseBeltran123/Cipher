7e0a8346f4a0cecaebd9d350c2adbf87b0c41286
JoseBeltran123 <josebelt407@gmail.com>
2025-10-06 22:46:35 -0600

    UpdateConexionaFireBase

---
+++ js/registro.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0gT4C0SOKmIeaulQjW1oh7YjrplWwQ10",
+  authDomain: "cipher-3bdc7.firebaseapp.com",
+  projectId: "cipher-3bdc7",
+  storageBucket: "cipher-3bdc7.firebasestorage.app",
+  messagingSenderId: "532286032747",
+  appId: "1:532286032747:web:aa9a1ab4737c51ee3a1a2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
